3d598a081b94ffb8501b3a9a91edf66baf5cba7a
ccordero <ccordero.007@gmail.com>
2019-02-25 17:36:58 +0000

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -18,14 +18,19 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) =>{
     console.log('New user connected');
     
-    socket.on('createMessage', (message) => {
-        console.log('createMessage', message);
-    });
+    // socket.emit('newMessage', {
+    //   from:'John',
+    //   text:'See you then',
+    //   createdAt: 123123
+    // });
     
-    socket.emit('newMessage', {
-       from:'John',
-       text:'See you then',
-       createdAt: 123123
+    socket.on('createMessage', (message) => {
+        // console.log('createMessage', message);
+        io.emit('newMessage',{
+           from: message.from,
+           text: message.text,
+           createtAt: new Date().getTime()
+        });
     });
     
     // socket.emit('newEmail', {
