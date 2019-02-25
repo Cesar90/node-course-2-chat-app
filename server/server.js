11ab3325c07fdc539a8295ed93c013ee3149dd51
ccordero <ccordero.007@gmail.com>
2019-02-25 18:00:51 +0000

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -18,18 +18,39 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) =>{
     console.log('New user connected');
     
+    socket.emit('newMessage',{
+       from:'Admin',
+       text:'Welcome to the chat app',
+       createdAt: new Date().getTime()
+    });
+    
+    //socket.broadcast.emit from Admin text New user joined
+    socket.broadcast.emit('newMessage', {
+       from:'Admin',
+       text:'New user joined',
+       createdAt: new Date().getTime()
+    });
+    
     // socket.emit('newMessage', {
     //   from:'John',
     //   text:'See you then',
     //   createdAt: 123123
     // });
     
+    //socket.emit from Admin text welcome to the chat app
+    //socket.broadcast.emit from admin text New user joined
+    
     socket.on('createMessage', (message) => {
         // console.log('createMessage', message);
-        io.emit('newMessage',{
-           from: message.from,
-           text: message.text,
-           createtAt: new Date().getTime()
+        // io.emit('newMessage',{
+        //   from: message.from,
+        //   text: message.text,
+        //   createtAt: new Date().getTime()
+        // });
+        socket.broadcast.emit('newMessage', {
+            from:message.from,
+            text:message.text,
+            createdAt:new Date().getTime()
         });
     });
     
